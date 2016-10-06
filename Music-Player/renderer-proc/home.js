7031fc2ada3f05cc8cbf031fd5a6edc24e9a5dec
Phap <d2phap@gmail.com>
2016-10-06 18:18:33 +0800

    controls: update style, effects

--- Music-Player/renderer-proc/home.js
+++ Music-Player/renderer-proc/home.js
@@ -1,8 +1,24 @@
 
+$(document).ready(() => {
 
-document.getElementById("btn").addEventListener("click", () => {
-    alert("Cliked")
-});
+    $("#btn").on("click", () => {
+        alert("Cliked")
+    })
+
+
+    // Button Play - onclick
+    $(".app-controls .btn-play").on("click", () => {
+        $(".app-controls .btn-play").addClass("hidden");
+        $(".app-controls .btn-pause").removeClass("hidden");
+    })
+    // Button Pause - onclick
+    $(".app-controls .btn-pause").on("click", () => {
+        $(".app-controls .btn-play").removeClass("hidden");
+        $(".app-controls .btn-pause").addClass("hidden");
+    })
+
+})
+
 
 
 
